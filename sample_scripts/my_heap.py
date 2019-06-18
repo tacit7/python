23712fc9aae5d29eaf7dce1e8c657c0d927b4976
Uriel Maldonado <uriel781@gmail.com>
2019-06-17 22:20:01 -0500

    Heap implementation

--- sample_scripts/my_heap.py
+++ sample_scripts/my_heap.py
@@ -1,4 +1,5 @@
 import math
+from IPython import embed
 
 class MyHeap(object):
     def __init__(self, lst):
@@ -14,7 +15,7 @@ class MyHeap(object):
         return 2 * parent_index + 2
 
     def parent_index(self, child_index):
-        return (child_index - 1) / 2
+        return math.floor((child_index - 1) / 2)
 
     def has_left_child(self, index):
         return self.left_child_index(index) < len(self.heap) - 1
@@ -23,7 +24,7 @@ class MyHeap(object):
         return self.right_child_index(index) < len(self.heap) - 1
 
     def has_parent(self, index):
-        return self.get_parent_index(index) >= 0
+        return self.parent_index(index) >= 0
 
     def left_child(self, index):
         return self.heap[self.left_child_index(index)]
@@ -39,7 +40,7 @@ class MyHeap(object):
         self.heap[index1] = self.heap[index2]
         self.heap[index2] = tmp
 
-    def poll(self):
+    def pop(self):
         element = self.heap[0]
         self.heap[0] = self.heap.pop
         self.heapifyDown()
@@ -48,3 +49,36 @@ class MyHeap(object):
     def add(self, element):
         self.heap.append(element)
         self.heapifyUp()
+
+    def heapify_up(self):
+        index = len(self.heap) - 1
+        while self.has_parent(index) and self.needs_to_become_parent(index):
+            self.swap(self.parent_index(index), index)
+            index = self.parent_index(index)
+
+        print(self.heap)
+
+    def heapify_down(self):
+        index = 0
+        while(self.has_left_child(index)): # you only need to check the left child since there is no right without the left
+            small_child_index = self.get_smallest_child_index(index)
+            if self.heap[index] < self.heap[small_child_index]:
+                break
+            else:
+                self.swap(index, small_child_index)
+                index = small_child_index
+
+
+        return self.heap
+
+
+    def needs_to_become_parent(self, index):
+        return self.parent(index) > self.heap[index]
+
+    def get_smallest_child_index(self, index):
+        child_index = self.left_child_index(index)
+
+        if self.has_right_child(index) and self.right_child(index) < self.left_child(child_index):
+            child_index = self.right_child_index(index)
+
+        return child_index
--- sample_scripts/test_my_heap.py
+++ sample_scripts/test_my_heap.py
@@ -32,3 +32,40 @@ class TestMyHeap(unittest.TestCase):
 
         self.assertEqual(h.parent(1), 0)
         self.assertEqual(h.parent(2), 0)
+
+    def test_needs_to_become_parent(self):
+        heap = [
+                 0,
+            1,        2,
+           11,12,  21, 22]
+        h = my_heap.MyHeap(heap)
+
+        self.assertFalse(h.needs_to_become_parent(6))
+        self.assertFalse(h.needs_to_become_parent(2))
+
+        heap = [
+                 0,
+            1,        2,
+            11,12,  21, 0]
+        h = my_heap.MyHeap(heap)
+
+        self.assertTrue(h.needs_to_become_parent(6))
+
+    def test_heapify_down(self):
+        heap = [
+                 0,
+            1,        2,
+            11,12,  21, 22]
+        h = my_heap.MyHeap(heap)
+
+        self.assertTrue(h.heapify_down())
+
+    def test_heapify_up(self):
+        heap = [
+                 0,
+            1,        22,
+            11,12,  21, 2]
+        h = my_heap.MyHeap(heap)
+
+        self.assertTrue(h.heapify_up())
+
